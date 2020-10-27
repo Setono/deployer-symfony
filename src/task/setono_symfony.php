1d05793af82f5ed1e43a873ec78433da7fba3f5d
Joachim Løvgaard <joachim@loevgaard.dk>
2020-10-27 11:02:32 +0100

    Add task

--- src/task/setono_symfony.php
+++ src/task/setono_symfony.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace Setono\Deployer\DotEnv;
 
+use function Deployer\commandExist;
+use function Deployer\locateBinaryPath;
+use function Deployer\run;
+use function Deployer\set;
 use function Deployer\task;
 
 task('symfony:binary', static function (): void {
+    set('bin/symfony', function () {
+        if (commandExist('symfony')) {
+            $binary = locateBinaryPath('symfony');
 
+            // todo: Outcomment this when this issue is fixed: https://github.com/symfony/cli/issues/352
+            // run(\Safe\sprintf('%s self:update --no-interaction'));
+
+            return $binary;
+        }
+
+        run('wget https://get.symfony.com/cli/installer -O - | bash');
+
+        return locateBinaryPath('symfony');
+    });
 })->desc('Checks if the symfony binary is installed. If not, it installs it');
